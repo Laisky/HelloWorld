ab1afddd317124500a3c4533d9c83fe4253faf41
Laisky <ppcelery@gmail.com>
2015-06-08 09:00:48 +0800

    #8: django 登陆 & 登出

--- py2/django/mysite/user/urls.py
+++ py2/django/mysite/user/urls.py
@@ -3,11 +3,12 @@
 
 from django.conf.urls import patterns, url
 
-from .views import Register, Login
+from .views import Register, Login, Authorized
 
 
 urlpatterns = patterns(
     '',
     url(r'^register/$', Register.as_view(), name='register'),
     url(r'^login/$', Login.as_view(), name='login'),
+    url(r'^authorized/$', Authorized.as_view(), name='login'),
 )
--- py2/django/mysite/user/views.py
+++ py2/django/mysite/user/views.py
@@ -7,7 +7,9 @@ from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
+from django.utils.decorators import method_decorator
 
 from .forms import LoginForm
 
@@ -16,6 +18,7 @@ logger = logging.getLogger(settings.LOG_NAME)
 
 
 class Register(View):
+    """注册"""
 
     def get(self, request):
         return render(request, 'user/register.html', {
@@ -42,6 +45,7 @@ class Register(View):
 
 
 class Login(View):
+    """登陆"""
 
     def get(self, request):
         return render(request, 'user/login.html', {
@@ -55,10 +59,24 @@ class Login(View):
         logger.debug('POST with {}'.format(request.POST))
 
         user = auth.authenticate(username=username, password=password)
-        if user is not None:
+        if user is not None and user.is_active:
             auth.login(request, user)
-            request.session['username'] = username
-            # return HttpResponseRedirect('/user/profile/')
-            return HttpResponse('Login in OK')
+            return HttpResponseRedirect('/user/authorized/')
         else:
             return HttpResponse('Login in Error')
+
+
+class Logout(View):
+    """登出"""
+
+    @method_decorator(login_required)
+    def post(self, request):
+        auth.logout(request)
+
+
+class Authorized(View):
+    """登陆后可见的内容"""
+
+    @method_decorator(login_required)
+    def get(self, request):
+        return render(request, 'user/authorized.html')
