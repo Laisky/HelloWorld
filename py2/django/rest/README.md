f762e6c9dbf25d717869da7b65bf7fa4bc5ec60a
Laisky <ppcelery@gmail.com>
2015-07-15 16:51:22 +0800

    #9 init

---
+++ py2/django/rest/README.md
@@ -0,0 +1,2 @@
+Django Project for RESTful
+===
---
+++ py2/django/rest/apis/urls.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8
+from __future__ import unicode_literals, absolute_import
+from django.conf.urls import url, include, patterns
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+)
