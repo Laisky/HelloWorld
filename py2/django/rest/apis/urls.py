26dff41f2b0fe20112ae3897170efebb5f9cce57
Laisky <ppcelery@gmail.com>
2015-07-16 15:17:10 +0800

    #9 router

--- py2/django/rest/apis/urls.py
+++ py2/django/rest/apis/urls.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, patterns, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
@@ -14,11 +15,16 @@ modal_plain = views.ModelViewSet.as_view({'get': 'plaintext'})
 model_detail = views.ModelViewSet.as_view({'get': 'retrieve', 'post': 'create'})
 model_list = views.ModelViewSet.as_view({'get': 'list', 'post': 'create'})
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'models', views.ModelViewSet)
+router.register(r'users', views.UserViewSet)
+
 
 urlpatterns = patterns(
     '',
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^$', views.api_root, name='root'),
     url(r'^models/(?P<pk>[0-9]+)/$', model_detail, name='model-detail'),
     url(r'^models/(?P<pk>[0-9]+)/plain/$', modal_plain, name='model-plain'),
     url(r'^models/$', model_list, name='model-list'),
--- py2/django/rest/apis/views.py
+++ py2/django/rest/apis/views.py
@@ -5,22 +5,12 @@ from __future__ import unicode_literals, absolute_import
 from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets, renderers
 from rest_framework.decorators import (
-    api_view, permission_classes, detail_route
+    permission_classes, detail_route
 )
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from .serializers import MySerializer, UserSerializer
 from .models import MyModel
-
-
-@api_view(('GET',))
-@permission_classes((permissions.AllowAny,))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'models': reverse('model-list', request=request, format=format)
-    })
 
 
 class UserViewSet(viewsets.ModelViewSet):
