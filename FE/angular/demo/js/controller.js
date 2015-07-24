c5fd6430e2975d2b613ac9867b05bc1377a86057
Laisky <ppcelery@gmail.com>
2015-07-24 17:56:21 +0800

    #14 fix demo

--- FE/angular/demo/js/controller.js
+++ FE/angular/demo/js/controller.js
@@ -1,9 +1,14 @@
+// controller.js
+
 (function() {
     var myApp = angular.module('myApp', []);
 
-
     myApp.controller('myController', ["$scope", function($scope) {
         // $scope 为该 controller 的命名空间
+        // $scope 中也可以指定函数
         $scope.greeting = "Welcome";
+        $scope.addDot = function() {
+            $scope.dot += '.';
+        };
     }]);
 })()
