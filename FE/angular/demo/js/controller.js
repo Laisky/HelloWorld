4366a7aa28c5b5e797a64c88e0868f8e3ad27ae6
Laisky <ppcelery@gmail.com>
2015-07-26 21:38:29 +0800

    #14 validators

--- FE/angular/demo/js/controller.js
+++ FE/angular/demo/js/controller.js
@@ -49,5 +49,38 @@ var myApp;
         myApp.controller("templateDemo", ["$scope", function($scope) {
 
         }]);
+
+
+        // -----------------------------------
+        // 校验整数
+        var INTEGER_REGEXP = /^\-?\d+$/;
+
+        // demo for validators
+        // 定义一个叫做 integer 的 directive
+        myApp.directive('integer', function() {
+            return {
+                require: 'ngModel',
+                link: function(scope, elm, attrs, ctrl) {
+                    // 设置自己的校验器
+                    // 接受两个参数 modelValue & viewValue
+                    ctrl.$validators.integer = function(modelValue, viewValue) {
+                        // 为空时不校验
+                        if (ctrl.$isEmpty(modelValue)) {
+                            return true;
+                        }
+
+                        // 注意这里使用了 viewValue 而不是 modelValue
+                        // 因为从 viewValue 到 modelValue 会经过 $parsers
+                        // 所以 viewValue 是字符串，而 modelValue 会被 parse 成相应的类型
+                        if (INTEGER_REGEXP.test(viewValue)) {
+                            return true;
+                        }
+
+                        // it is invalid
+                        return false;
+                    };
+                }
+            };
+        });
     }
 })()
