c1956b776b7aa52ab67cc2af010f7f49128a7f8d
Laisky <ppcelery@gmail.com>
2015-07-26 15:16:26 +0800

    #14 service for constructior

--- FE/angular/demo/js/controller.js
+++ FE/angular/demo/js/controller.js
@@ -1,32 +1,42 @@
 // controller.js
 
+"use strict;"
+
+var myApp;
+
 (function() {
-    var myApp = angular.module('myApp', []);
+    myApp = new MyApp();
 
-    // -----------------------------------
-    // demo for controller
-    myApp.controller('myController', ["$scope", function($scope) {
-        // $scope 为该 controller 的命名空间
-        // $scope 中也可以指定函数
-        $scope.greeting = "Welcome";
-        $scope.addDot = function() {
-            $scope.dot += '.';
-        };
-    }]);
+    function MyApp() {
+        this.myApp = angular.module('myApp', []);
 
-    // -----------------------------------
-    // demo for service
-    // 将自定义的 service 也添加进依赖注入（DI）的列表中
-    myApp.controller("serviceDemo", ["$scope", "myService", function($scope, myService) {
-        $scope.serviceShow = function(userInput) {
-            myService(userInput);
-        };
-    }]);
-    // 创建 service
-    // service 返回的是一个函数（实例）
-    myApp.factory("myService", ["$window", function($win) {
-        return function (msg) {
-            $win.alert(msg);
-        }
-    }]);
+        var myApp = this.myApp;
+
+        // -----------------------------------
+        // demo for controller
+        myApp.controller('myController', ["$scope", function($scope) {
+            // $scope 为该 controller 的命名空间
+            // $scope 中也可以指定函数
+            $scope.greeting = "Welcome";
+            $scope.addDot = function() {
+                $scope.dot += '.';
+            };
+        }]);
+
+        // -----------------------------------
+        // demo for service
+        // 将自定义的 service 也添加进依赖注入（DI）的列表中
+        myApp.controller("serviceDemo", ["$scope", "myService", function($scope, myService) {
+            $scope.serviceShow = function(userInput) {
+                myService(userInput);
+            };
+        }]);
+        // 创建 service
+        // service 返回的是一个函数（实例）
+        myApp.factory("myService", ["$window", function($win) {
+            return function(msg) {
+                $win.alert(msg);
+            }
+        }]);
+    }
 })()
