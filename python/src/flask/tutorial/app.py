5165574fe5ac7ba0d2bd26124d58e9aa694960dd
Laisky <ppcelery@gmail.com>
2015-08-02 20:02:27 +0800

    #13 flask quick start

--- python/src/flask/tutorial/app.py
+++ python/src/flask/tutorial/app.py
@@ -1,21 +1,56 @@
+import os
+import sys
 import logging
 
-from flask import Flask, render_template, request
+from flask import (
+    Flask, render_template, request, url_for,
+    make_response, session, flash,
+)
 
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+# app settings
 app = Flask(__name__)
+app.secret_key = os.urandom(24)  # for session
 
 
-@app.route("/index")
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    resp = make_response(render_template('index.html'))
+    resp.set_cookie('username', 'laisky')
+    session['username'] = 'laisky'
+    flash('holly chirst!')
+    return resp
+
+
+@app.errorhandler(404)
+def pagenotfound(error):
+    app.logger.debug(error)
+    # return (response, status, headers)
+    # resp = make_response(render_template('error.html'), 404)
+    return 'Page Not Found!!!', 404
+
+
+# 类型可选为 int, float, path
+@app.route('/t/<int:pid>', methods=['GET'])
+def retrieve_posts(pid):
+    return 'You got the post {}'.format(pid)
 
 
 def main():
     app.run(port=27855, debug=True)
 
 
+def test():
+    # 模拟 request 上下文，便与编写单元测试
+    with app.test_request_context('index', method='POST'):
+        # request.args 可以获取到 url 中的参数
+        # request.form 可以获取到 POST 和 PUT 的参数
+        # request.files 可以获取到 POST 的文件
+        print('request: {}'.format(request))
+        print(url_for('index'))
+        print(url_for('retrieve_posts', pid=233))
+
+
 if __name__ == '__main__':
+    test()
     main()
