b7b0fa02b62df6fdcbec7e0163020e769f8ef9be
Laisky <ppcelery@gmail.com>
2015-08-04 18:02:01 +0800

    #13 args stash

--- python/src/flask/rest/app.py
+++ python/src/flask/rest/app.py
@@ -1,3 +1,7 @@
+import datetime
+
+import pytz
+import werkzeug
 from flask import Flask
 from flask_restful import (
     Resource, Api, reqparse,
@@ -12,7 +16,16 @@ class PostHandler(Resource):
 
     def req_parses(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('sort', type=int, help='is sorted!')
+        # Look only in the querystring
+        parser.add_argument('sort', type=int, help='is sorted!', location=['args', 'values'])
+        # Look only in the POST body
+        parser.add_argument('name', type=int, location='form')
+        # From the request headers
+        parser.add_argument('User-Agent', location='headers')
+        # From http cookies
+        parser.add_argument('session_id', location='cookies')
+        # From file uploads
+        parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
         args = parser.parse_args()
         return args
 
@@ -32,10 +45,12 @@ class Resp():
     def __init__(self, id_):
         self.id = id_
         self.num = 100
+        self.date = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai'))
 
 RESOURCE_FIELD = {
     'id': fields.String,
     'num': fields.Integer,
+    'date': fields.DateTime(dt_format='rfc822'),
 }
 
 
