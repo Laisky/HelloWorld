3937fe80edccb6ef65d62609a004cd251f128f36
Laisky <ppcelery@gmail.com>
2015-08-04 16:37:19 +0800

    #13 marshal_with

--- python/src/flask/rest/app.py
+++ python/src/flask/rest/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import (
+    Resource, Api, reqparse,
+    fields, marshal_with
+)
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,17 +10,47 @@ api = Api(app)
 
 class PostHandler(Resource):
 
+    def req_parses(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('sort', type=int, help='is sorted!')
+        args = parser.parse_args()
+        return args
+
     def get(self, id_=None):
-        return {'get': id_ or 'list'}
+        args = self.req_parses()
+        return {
+            'get': id_ or 'list',
+            'args': args,
+        }
 
     def post(self, id_):
         return {'post': id_}
 
 
+class Resp():
+
+    def __init__(self, id_):
+        self.id = id_
+        self.num = 100
+
+RESOURCE_FIELD = {
+    'id': fields.String,
+    'num': fields.Integer,
+}
+
+
+class ResourceHandler(Resource):
+
+    @marshal_with(RESOURCE_FIELD)
+    def get(self, id_):
+        return Resp(id_)
+
+
 api.add_resource(PostHandler, '/p/<string:id_>/', '/p/')
+api.add_resource(ResourceHandler, '/t/<string:id_>/', '/t/')
 
 if __name__ == '__main__':
     app.run(
-        # debug=True,
+        debug=True,
         port=27855
     )
