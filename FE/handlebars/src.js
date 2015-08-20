3b38445c3efa003fa79af695dd8d6e8217285504
Laisky <ppcelery@gmail.com>
2015-08-20 14:23:57 +0800

    + handlebars: filter wrapper & each

--- FE/handlebars/src.js
+++ FE/handlebars/src.js
@@ -10,28 +10,61 @@
         weapons: ['knife', 'gun']
     };
 
+    /**
+     * 渲染并填充
+     */
+    function renderTemplate(sourceId, targetId, context) {
+        context = context || globalContext;
+        let source = $('#' + sourceId).html();
+        let template = Handlebars.compile(source);
+        let html = template(context);
+        $('#' + targetId).html(html);
+    }
+
 
     /**
      * app 1: expression
      */
-    (function() {
-        // 普通 expression
-        let source = $('#tpl-exp').html();
-        let template = Handlebars.compile(source);
-        let html = template(globalContext);
-        $('#exp-1').html(html);
-    })();
+    renderTemplate('tpl-exp', 'exp-1');
 
 
     /**
      * app 2: escape
      */
-    (function() {
-        // without escape
-        let source = $('#tpl-escape').html();
-        let template = Handlebars.compile(source);
-        let html = template(globalContext);
-        $('#escape-1').html(html)
-    })();
+    renderTemplate('tpl-escape', 'escape-1');
+
+
+    /**
+     * app3: helper
+     */
+    // 简单的 helper
+    Handlebars.registerHelper('my-helper', function(options) {
+        return options.fn({
+            body: '毛也没有'
+        });
+    });
+    renderTemplate('tpl-helper', 'helper');
+
+    // helper wrapper
+    Handlebars.registerHelper('my-helper-wrapper', function(options) {
+        return '<p style="color: red;">' + options.fn(this) + '</p>';
+    })
+    renderTemplate('tpl-helper-wrapper', 'helper-wrapper');
+
+    // 包含 with 的 helper
+    Handlebars.registerHelper('my-helper-with', function(options) {
+        let context = {
+            title: '我是标题党',
+            content: {
+                name: '看板娘',
+                sex: '妹纸'
+            }
+        };
+        return options.fn(context);
+    });
+    renderTemplate('tpl-helper-with', 'helper-with');
+
+    // helper each
+    renderTemplate('tpl-helper-each', 'helper-each');
 
 })();
