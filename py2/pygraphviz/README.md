90e73b3dd1b6b8ecad18c071adfe8d3e8921d004
Laisky <ppcelery@gmail.com>
2015-08-25 08:52:49 +0800

    * python3: update setup

--- py2/pygraphviz/README.md
+++ py2/pygraphviz/README.md
@@ -6,6 +6,7 @@
 ## Reference
 
 - [pygraphviz 笔记](http://blog.laisky.com/p/pygraphviz/)
+- [pygraphviz tutorial](http://vdisk.weibo.com/s/G3pH4ZjwAfcH)
 - https://github.com/pygraphviz/pygraphviz
 - http://www.graphviz.org/doc/info/attrs.html
 - http://www.graphviz.org/doc/info/shapes.html
--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
+from pip.download import PipSession
 
 
-requires = [str(i.req) for i in parse_requirements('requirements.txt')
+requires = [str(i.req) for i in parse_requirements('requirements.txt', session=PipSession())
             if i.req is not None]
 
 
 setup(name='src',
-      version='0.1',
+      version='0.2',
       packages=find_packages('src'),
       install_requires=requires)
