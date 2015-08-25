7bbbbc6c6e783c2f357c30cf68e3dc40af673e80
Laisky <ppcelery@gmail.com>
2015-08-25 13:34:50 +0800

    + yeoman

---
+++ FE/angular/ui-route/js/main.js
@@ -0,0 +1,35 @@
+(function() {
+    var myApp = angular.module('myApp', ['ui.router']);
+
+
+    myApp.config(function($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/state1");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('state1', {
+                url: "/state1",
+                templateUrl: "state1.html"
+            })
+            .state('state1.list', {
+                url: "/list",
+                templateUrl: "state1.list.html",
+                controller: function($scope) {
+                    $scope.items = ["A", "List", "Of", "Items"];
+                }
+            })
+            .state('state2', {
+                url: "/state2",
+                templateUrl: "state2.html"
+            })
+            .state('state2.list', {
+                url: "/list",
+                templateUrl: "state2.list.html",
+                controller: function($scope) {
+                    $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            });
+    });
+})()
