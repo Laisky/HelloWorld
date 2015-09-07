17799f2777425a3a0e665727287583820e02c6a3
Laisky <ppcelery@gmail.com>
2015-09-07 12:59:37 +0800

    + javascript: 简单的 setTimeout & setInterval

--- javascript/wheels/asynchronous.js
+++ javascript/wheels/asynchronous.js
@@ -3,10 +3,77 @@
  * Mon Sep  7 10:56:17 2015
  */
 
-'use strict';
 
-
+/**
+ * javascript 里内置的一些定时器
+ */
 describe('定时器', function() {
+
+    beforeEach(function() {
+        jasmine.clock().install();
+    });
+
+    afterEach(function() {
+        jasmine.clock().uninstall();
+    });
+
+    it('简单的 setTimeout', function() {
+        var result = [];
+        setTimeout(function() {
+            result.push(1);
+        }, 100);
+
+        jasmine.clock().tick(101);
+        expect(result).toEqual([1]);
+    });
+
+    it('简单的 setInterval', function() {
+        var result = '';
+        var myInter = setInterval(function() {
+            result += 'ni';
+        }, 100);
+        jasmine.clock().tick(501);
+        clearInterval(myInter);
+        expect(result).toEqual('ninininini');
+    })
+
+    /**
+     * 下面的各种情形在 setTimeout 和 setInterval 中同样适用，就不各自重复写了
+     */
+    it('绑定对象方法', function() {
+        var result;
+        window.myVar = 10; // 设定全局变量
+
+        function MyObj() {
+            this.myVar = 5;
+            this.getVar = function() {
+                result = this.myVar;
+            }
+        }
+
+        var myObj = new MyObj();
+
+        // 正常调用时，得到的是对象内的 this.myVar
+        myObj.getVar()
+        expect(result).toEqual(5);
+
+        // 直接将对象方法绑定到 setTimeout 的话，this 会指向全局
+        // 如果使用了严格模式，所以 this 会指向 null，导致报错
+        // setInterval 有相同的问题
+        setTimeout(myObj.getVar, 100);
+        jasmine.clock().tick(101);
+        expect(result).toEqual(10);
+
+
+        // 解决的办法是将对象方法嵌套在函数内
+        jasmine.clock().tick(0);
+        setTimeout(function() {
+            myObj.getVar();
+        }, 100);
+        jasmine.clock().tick(101);
+        expect(result).toEqual(5); // 现在就能得到正确的值
+    });
+
 
 });
 
