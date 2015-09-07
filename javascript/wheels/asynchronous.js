4c8d45e5e36f83a06abf56dde09260bd0157e852
Laisky <ppcelery@gmail.com>
2015-09-07 11:00:10 +0800

    + javascript: asynchronous init

---
+++ javascript/wheels/asynchronous.js
@@ -0,0 +1,21 @@
+/**
+ * 学习一下 javascript 和 jquery 里的各种异步
+ * Mon Sep  7 10:56:17 2015
+ */
+
+'use strict';
+
+
+describe('定时器', function() {
+
+});
+
+
+describe('Deffered', function() {
+
+});
+
+
+describe('Promisse', function() {
+
+});
