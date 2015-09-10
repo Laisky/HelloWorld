0e4c47e7942b5f1959e7e64bb0c309528a4d6541
Laisky <ppcelery@gmail.com>
2015-09-10 09:09:37 +0800

    * js: canvas gradient

--- FE/canvas/src.js
+++ FE/canvas/src.js
@@ -9,7 +9,7 @@
     ctx.moveTo(20, 20); // 设置路径起点，坐标为(20,20)
     ctx.lineTo(200, 20); // 绘制一条到(200,20)的直线
     ctx.lineWidth = 1.0; // 设置线宽
-    ctx.strokeStyle = "#CC0000"; // 设置线的颜色
+    ctx.strokeStyle = '#CC0000'; // 设置线的颜色
     ctx.stroke(); // 进行线的着色，这时整条线才变得可见
 
     // 绘制实心矩形
@@ -26,24 +26,32 @@
     ctx.clearRect(100, 10, 20, 500);
 
     // 绘制文本
-    ctx.font = "Bold 20px Arial"; // 设置字体
-    ctx.textAlign = "left"; // 设置对齐方式
-    ctx.fillStyle = "#008600"; // 设置填充颜色
-    ctx.fillText("Hello!", 20, 270); // 设置字体内容，以及在画布上的位置
-    ctx.strokeText("Hello!", 120, 270); // 绘制空心字
+    ctx.font = 'Bold 20px Arial'; // 设置字体
+    ctx.textAlign = 'left'; // 设置对齐方式
+    ctx.fillStyle = '#008600'; // 设置填充颜色
+    ctx.fillText('Hello!', 20, 270); // 设置字体内容，以及在画布上的位置
+    ctx.strokeText('Hello!', 120, 270); // 绘制空心字
 
     // 绘制圆形
     // void ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
     // 实心圆
     ctx.beginPath();
     ctx.arc(70, 370, 50, 0, Math.PI * 2, true);
-    ctx.fillStyle = "green";
+    ctx.fillStyle = 'green';
     ctx.fill();
 
     // 空心圆
     ctx.beginPath();
     ctx.arc(70, 500, 50, 0, Math.PI * 2, true);
     ctx.lineWidth = 1.0;
-    ctx.strokeStyle = "#000";
+    ctx.strokeStyle = '#000';
     ctx.stroke();
+
+    // 渐变色
+    // CanvasGradient ctx.createLinearGradient(x0, y0, x1, y1);
+    var myGradient = ctx.createLinearGradient(20, 600, 200, 650);
+    myGradient.addColorStop(0, 'red');
+    myGradient.addColorStop(1, 'blue');
+    ctx.fillStyle = myGradient;
+    ctx.fillRect(20, 600, 180, 50)
 })()
