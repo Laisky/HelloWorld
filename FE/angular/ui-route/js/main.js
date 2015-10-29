39721ba8371d8123e50fa9b53048182351b30f90
Laisky <ppcelery@gmail.com>
2015-10-29 13:40:33 +0800

    * fix angular

--- FE/angular/ui-route/js/main.js
+++ FE/angular/ui-route/js/main.js
@@ -5,7 +5,7 @@
     myApp.config(function($stateProvider, $urlRouterProvider) {
         //
         // For any unmatched url, redirect to /state1
-        $urlRouterProvider.otherwise("/state1");
+        $urlRouterProvider.otherwise("/");
         //
         // Now set up the states
         $stateProvider
