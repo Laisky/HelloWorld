3f46b35bee5ecb1afb7c83337dddca466a5bd40f
Laisky <ppcelery@gmail.com>
2019-08-06 17:42:10 +0800

    fix: py2 & py3

--- golang/src/demo/gocache-demo/main_test.go
+++ golang/src/demo/gocache-demo/main_test.go
@@ -1,6 +1,23 @@
+/*
+BenchmarkKVS/go-cache_insert-4           1000000             25018 ns/op             343 B/op          5 allocs/op
+BenchmarkKVS/go-cache_get-4              2000000              8069 ns/op              64 B/op          2 allocs/op
+BenchmarkKVS/go-cache_count-4           50000000               302 ns/op               0 B/op          0 allocs/op
+BenchmarkKVS/sync.map_insert-4           1000000             17755 ns/op             322 B/op          9 allocs/op
+BenchmarkKVS/sync.map_get-4              2000000              9097 ns/op              64 B/op          2 allocs/op
+BenchmarkKVS/sync.map_count-4                 50         552671238 ns/op             172 B/op          5 allocs/op
+
+BenchmarkInt64KVS/go-cache_insert-4      1000000             19597 ns/op              84 B/op          4 allocs/op
+BenchmarkInt64KVS/go-cache_get-4        10000000              1848 ns/op               0 B/op          0 allocs/op
+BenchmarkInt64KVS/go-cache_count-4      30000000               384 ns/op               0 B/op          0 allocs/op
+BenchmarkInt64KVS/sync.map_insert-4      1000000             14937 ns/op             250 B/op          7 allocs/op
+BenchmarkInt64KVS/sync.map_get-4        10000000              1135 ns/op               0 B/op          0 allocs/op
+BenchmarkInt64KVS/sync.map_count-4            50         330416714 ns/op             103 B/op          3 allocs/op
+*/
+
 package main_test
 
 import (
+	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -10,17 +27,6 @@ import (
 	"github.com/patrickmn/go-cache"
 )
 
-/*
-go test . -race -bench "K" -benchtime=10s -benchmem
-goos: darwin
-goarch: amd64
-pkg: github.com/Laisky/HelloWorld/golang/src/demo/gocache-demo
-
-BenchmarkKVS/go-cache_insert-4            500000             26164 ns/op             341 B/op          5 allocs/op
-BenchmarkKVS/go-cache_get-4              1000000             12395 ns/op              64 B/op          2 allocs/op
-BenchmarkKVS/sync.map_insert-4            500000             25238 ns/op             321 B/op          9 allocs/op
-BenchmarkKVS/sync.map_get-4              1000000             12881 ns/op              64 B/op          2 allocs/op
-*/
 func BenchmarkKVS(b *testing.B) {
 	goCache := cache.New(5*time.Minute, 10*time.Minute)
 	b.Run("go-cache insert", func(b *testing.B) {
@@ -31,6 +37,11 @@ func BenchmarkKVS(b *testing.B) {
 	b.Run("go-cache get", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			goCache.Get(utils.RandomStringWithLength(10))
+		}
+	})
+	b.Run("go-cache count", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			goCache.ItemCount()
 		}
 	})
 
@@ -45,5 +56,49 @@ func BenchmarkKVS(b *testing.B) {
 			syncMap.Load(utils.RandomStringWithLength(10))
 		}
 	})
+	b.Run("sync.map count", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			syncMap.Range(func(k, v interface{}) bool {
+				return true
+			})
+		}
+	})
+}
 
+func BenchmarkInt64KVS(b *testing.B) {
+	goCache := cache.New(5*time.Minute, 10*time.Minute)
+	b.Run("go-cache insert", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			goCache.Set(string(rand.Int63()), utils.RandomStringWithLength(10), cache.DefaultExpiration)
+		}
+	})
+	b.Run("go-cache get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			goCache.Get(string(rand.Int63()))
+		}
+	})
+	b.Run("go-cache count", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			goCache.ItemCount()
+		}
+	})
+
+	syncMap := sync.Map{}
+	b.Run("sync.map insert", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			syncMap.Store(rand.Int63(), utils.RandomStringWithLength(10))
+		}
+	})
+	b.Run("sync.map get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			syncMap.Load(rand.Int63())
+		}
+	})
+	b.Run("sync.map count", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			syncMap.Range(func(k, v interface{}) bool {
+				return true
+			})
+		}
+	})
 }
--- py34/README.md
+++
@@ -1,6 +0,0 @@
-Fun in Python3.x
----
-
-Python 是门好语言
-
-写过篇介绍文：https://blog.laisky.com/p/whats-new-in-python3-4/
