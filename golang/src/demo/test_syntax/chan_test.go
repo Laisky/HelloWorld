b9538dff4507b6f8570ccc3b8503b870ce0e4578
Laisky <ppcelery@gmail.com>
2020-01-16 15:49:43 +0800

    + channel

--- golang/src/demo/test_syntax/chan_test.go
+++ golang/src/demo/test_syntax/chan_test.go
@@ -26,3 +26,23 @@ func TestClocsChan(t *testing.T) {
 	}
 
 }
+
+func TestSelectChan(t *testing.T) {
+	chan1 := make(chan int, 100000)
+	chan2 := make(chan int, 100000)
+	chan3 := make(chan int, 100000)
+
+	for i := 0; i < 100; i++ {
+		select {
+		case chan1 <- i:
+		case chan2 <- i:
+		default:
+			chan3 <- i
+		}
+	}
+
+	t.Logf("len chan1: %v", len(chan1))
+	t.Logf("len chan2: %v", len(chan2))
+	t.Logf("len chan3: %v", len(chan3))
+	t.Error()
+}
