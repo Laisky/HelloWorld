2e60dbf550e8b9b6e7b7b6d3e391de69ddf8ac67
Laisky.Cai <public@laisky.com>
2022-11-09 09:58:23 +0000

    python 能跑

--- sgx/gramine/pandas/demo.py
+++ sgx/gramine/pandas/demo.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # pandas.__version__ == '1.4.2'
 
-import pandas as pd 
+import pandas as pd
+
+print("run...")
+
 data = pd.DataFrame([[1,2,3,4],[2,3,4,5]])
 data_test = pd.concat([data, data])
 if data_test.shape == (4,4):
 	print("Succeed!")
+
+
+
+
+print("exit")
