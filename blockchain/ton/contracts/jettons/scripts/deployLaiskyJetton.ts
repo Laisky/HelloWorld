3b350c5f333780edc796830834f29ccc1714e2ee
Laisky.Cai <github@laisky.com>
2024-07-26 05:51:10 +0000

    feat: Add custom jetton contract with minting functionality
    This commit adds a custom jetton contract called LaiskyJetton with the ability to mint new tokens and send them to a specified wallet owner. It also includes additional functionality such as updating the content of the jetton and disabling further minting. The contract adheres to the standard interface defined by the Jetton trait, ensuring interoperability with other jettons on TON. The commit also includes updates to the deployment script for the LaiskyJetton contract.

--- blockchain/ton/contracts/jettons/scripts/deployLaiskyJetton.ts
+++ blockchain/ton/contracts/jettons/scripts/deployLaiskyJetton.ts
@@ -1,9 +1,15 @@
-import { toNano } from '@ton/core';
-import { LaiskyJetton } from '../wrappers/LaiskyJetton';
-import { NetworkProvider } from '@ton/blueprint';
+import { Address, toNano, beginCell } from '@ton/core';
+import { LaiskyJetton, Mint } from '../wrappers/LaiskyJetton';
+import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const laiskyJetton = provider.open(await LaiskyJetton.fromInit());
+    let myAddress = Address.parse('0QARnduCSjymI91urfHE_jXlnTHrmr0e4yaPubtPQkgy553b')
+
+    const laiskyJetton = provider.open(await LaiskyJetton.fromInit(
+        myAddress,
+        beginCell().storeUint(666, 10).endCell(),
+        toNano(1000)
+    ));
 
     await laiskyJetton.send(
         provider.sender(),
@@ -11,8 +17,9 @@ export async function run(provider: NetworkProvider) {
             value: toNano('0.05'),
         },
         {
-            $$type: 'Deploy',
-            queryId: 0n,
+            $$type: 'Mint',
+            amount: toNano('50'),
+            receiver: myAddress,
         }
     );
 
