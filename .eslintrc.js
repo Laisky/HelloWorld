ccefcf0431dd1d10780740f5af6b516de686be37
Laisky.Cai <github@laisky.com>
2024-08-02 03:34:01 +0000

    chore: Add ESLint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: 'standard',
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [
+                '.eslintrc.{js,cjs,ts}'
+            ],
+            parserOptions: {
+                sourceType: 'script'
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4],
+        'no-lone-blocks': 'off',
+        semi: 'off',
+        egegeg: 'off'
+    }
+}
