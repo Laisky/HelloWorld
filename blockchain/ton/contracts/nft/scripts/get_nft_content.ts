ec5e8a916611a97db6383c647e46e902d12a29b4
Laisky.Cai <github@laisky.com>
2024-08-31 01:23:13 +0000

    Optimize contract logic and nativeReserve calls for better performance

--- blockchain/ton/contracts/nft/scripts/get_nft_content.ts
+++ blockchain/ton/contracts/nft/scripts/get_nft_content.ts
@@ -1,21 +1,36 @@
 import { toNano, beginCell, Address } from '@ton/core';
 import { NftCollection } from '../build/Nft/tact_NftCollection';
+import { NftItem } from '../build/Nft/tact_NftItem';
 import { NetworkProvider } from '@ton/blueprint';
 
 
 export async function run(provider: NetworkProvider) {
-    const collectionContract = provider.sender().address!!
+    const myAddr = provider.sender().address!!
 
     const masterContract = provider.open(await NftCollection.fromInit(
-        collectionContract,
+        myAddr,
         "https://s3.laisky.com/public/nft/ton-demo/"
     ));
+    const itemContract = provider.open(await NftItem.fromInit(
+        masterContract.address,
+        BigInt("0")
+    ));
 
+    // get nft collection data
+    const nftCollectionData = await masterContract.getGetCollectionData();
+    console.log(`collectionContent: ${nftCollectionData.collectionContent.asSlice().loadStringTail()}`);
+    // https://s3.laisky.com/public/nft/ton-demo/collection.json
+
+    // get nft item's data
+    const nftItemData = await itemContract.getGetNftData();
+    console.log(`individualContent: ${nftItemData.individualContent.asSlice().loadStringTail()}`);
+    // 0.json
+
+    // get nft metadata
     const result = await masterContract.getGetNftContent(
         BigInt("0"),
-        beginCell().storeStringTail("0").endCell()
+        beginCell().storeStringTail("0.json").endCell()
     )
-
-    console.log(result.toBoc().toString());
-    // ��rA2`https://s3.laisky.com/public/nft/ton-demo/0.jsonh��
+    console.log(`getGetNftContent: ${result.asSlice().loadStringTail()}`);
+    // https://s3.laisky.com/public/nft/ton-demo/0.json
 }
